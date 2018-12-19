069910e4c1af819193821e8daf1040e94d53a429
Gouthami Meadipally <gouthamimeadipally93@gmail.com>
2018-12-19 13:31:35 +0530

    Create Employee.java

---
+++ demo/src/main/java/com/nt/beans/Employee.java
@@ -0,0 +1,14 @@
+package com.nt.beans;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author GOUTHAMI
+ */
+@Component
+public class Employee {
+    private void sayHello(){
+    System.out.println("Hello from EMployee");
+    }
+}
